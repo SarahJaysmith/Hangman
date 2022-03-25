1f713f3ac36eaac4afcfedd3c2f872fa96f50a34
Sarah Jaysmith <38087068+SarahJaysmith@users.noreply.github.com>
2022-03-25 01:23:35 -0700

    Delete code directory

--- code/scripts/header.js
+++
@@ -1,8 +0,0 @@
-// constants
-
-const userName              = "Sarah Jaysmith";
-const assignment            = "Comp 2132 - Final Project";
-
-// print out the heading and initial message 
-$("#userName").text(userName);
-$("#assignment").text(assignment);
